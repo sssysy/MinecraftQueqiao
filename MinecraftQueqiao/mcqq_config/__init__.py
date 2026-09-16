39d8b3ce558eef107359b6e0dff584161ffb1a54
sssysy <sssysy@vip.qq.com>
2026-09-16 20:32:11 +0800

    fix: 调整设置参数

--- MinecraftQueqiao/mcqq_config/__init__.py
+++ MinecraftQueqiao/mcqq_config/__init__.py
@@ -97,9 +97,9 @@ CONFIG_DEFAULT: Dict[str, GSC] = {
         options=["正面大脸"],
     ),
     "ms_client_id": GsStrConfig(
-        "微软应用客户端ID",
-        "Azure 应用注册的客户端(应用程序) ID，用于微软账号登录",
-        "86cda923-6960-40a4-b209-cf2f05871292",
+        "Azure 客户端(应用程序) ID",
+        "用于登录命令调用 Microsoft 验证，需要申请应用白名单 (详见 https://aka.ms/AppRegInfo)，普通用户保持默认即可",
+        "2591d784-2759-4ace-8858-24620f9e460b",
     ),
     "ms_login_timeout": GsIntConfig(
         "微软登录超时时间(秒)",
